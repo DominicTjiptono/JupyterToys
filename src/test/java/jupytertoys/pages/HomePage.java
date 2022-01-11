1e4bce55a77d8a73669b1cd18874d4bf5e52d4f8
Dominic Tjiptono <dtjiptono@planittesting.com>
2022-01-12 11:04:28 +1300

    Managed to get test case 4 working.

--- src/test/java/jupytertoys/pages/HomePage.java
+++ src/test/java/jupytertoys/pages/HomePage.java
@@ -7,9 +7,21 @@ public class HomePage {
 
     // Class attributes
     private WebDriver driver;
+    private By usernameField = By.id("loginUserName");
+    private By passwordField = By.id("loginPassword");
+    private By loginButton = By.className("btn-primary");
+    private By usernameMenuBar = By.id("nav-user");
 
     public HomePage(WebDriver driver){
         this.driver = driver;
+    }
+
+    public void setUsername(String username){
+        driver.findElement(usernameField).sendKeys(username);
+    }
+
+    public void setPassword(String password) {
+        driver.findElement(passwordField).sendKeys(password);
     }
 
     public HomePage clickHomePage() {
@@ -27,14 +39,21 @@ public class HomePage {
         return new ShopPage(driver);
     }
 
-    public LoginPage clickLoginPage() {
+    public void clickLoginPage() {
         clickButton("#nav-login");
-        return new LoginPage(driver);
     }
 
     public CartPage clickCartPage() {
         clickButton("#nav-cart");
         return new CartPage(driver);
+    }
+
+    public String getUsernameInMenuBar() {
+        return driver.findElement(usernameMenuBar).getText();
+    }
+
+    public void clickLoginButton(){
+        driver.findElement(loginButton).click();
     }
 
     // Creating a method to click a button in the page.
--- src/test/java/jupytertoys/tests/LoginTests.java
+++ src/test/java/jupytertoys/tests/LoginTests.java
@@ -1,13 +1,22 @@
 package jupytertoys.tests;
 
-import jupytertoys.tests.BaseTests;
-import jupytertoys.pages.LoginPage;
 import org.testng.annotations.Test;
+import static org.testng.Assert.*;
 
 public class LoginTests extends BaseTests {
     @Test
     public void testCase04() {
         // Clicking the login menu from the home page
-        LoginPage loginPage = homePage.clickLoginPage();
+        homePage.clickLoginPage();
+
+        // Entering username "anyvaluewilldo" and password "letmein"
+        homePage.setUsername("anyvaluewilldo");
+        homePage.setPassword("letmein");
+
+        // Clicking the login button
+        homePage.clickLoginButton();
+
+        // Verifying that the username appears in the menu bar
+        assertEquals(homePage.getUsernameInMenuBar(), "anyvaluewilldo");
     }
 }
