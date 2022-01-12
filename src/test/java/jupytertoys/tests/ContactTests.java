5fd5d3a51676a35fb6c5e565144caf3fc306b32f
Dominic Tjiptono <dtjiptono@planittesting.com>
2022-01-12 15:03:05 +1300

    Removed comments and added assertion messages for test cases 1 to 3.

--- src/test/java/jupytertoys/tests/ContactTests.java
+++ src/test/java/jupytertoys/tests/ContactTests.java
@@ -8,61 +8,42 @@ import static org.testng.Assert.*;
 public class ContactTests extends BaseTests {
     @Test
     public void testCase01() {
-        // Going to the contact page
         ContactPage contactPage = homePage.clickContactPage();
-
-        // Populating the email field with "thisisnotavalidemail"
         contactPage.setEmail("thisisnotavalidemail");
-
-        // Clicking the submit button
         contactPage.clickSubmitButton();
-
-        // Asserting that the email error is displayed with text "Please enter a valid email"
-        assertEquals(contactPage.getEmailErrorText(), "Please enter a valid email");
+        assertEquals(contactPage.getEmailErrorText(), "Please enter a valid email",
+                "Email error occurs");
     }
 
     @Test
     public void testCase02() {
-        // Going to the contact page
         ContactPage contactPage = homePage.clickContactPage();
-
-        // Clicking the submit button
         contactPage.clickSubmitButton();
-
-        // Ensuring that the errors "Forename is required",
-        // "Email is required", and "Message is required" are shown.
-        assertEquals(contactPage.getForenameErrorText(), "Forename is required");
-        assertEquals(contactPage.getEmailErrorText(), "Email is required");
-        assertEquals(contactPage.getMessageErrorText(), "Message is required");
-
-        // Populating mandatory fields
+        assertEquals(contactPage.getForenameErrorText(), "Forename is required",
+                "Forename error occurs");
+        assertEquals(contactPage.getEmailErrorText(), "Email is required",
+                "Email error occurs");
+        assertEquals(contactPage.getMessageErrorText(), "Message is required",
+                "Message error occurs");
         contactPage.setForename("ex");
         contactPage.setEmail("example@gmail.com");
         contactPage.setMessage("I am new!");
-
-        // Ensuring that the mandatory errors are no longer displayed.
-        String forenameError = contactPage.getForenameErrorText();
-        String emailError = contactPage.getEmailErrorText();
-        String messageError = contactPage.getMessageErrorText();
-        assertEquals(forenameError, "");
-        assertEquals(emailError, "");
-        assertEquals(messageError, "");
+        assertEquals(contactPage.getForenameErrorText(), "",
+                "Forename error does not exist");
+        assertEquals(contactPage.getEmailErrorText(), "",
+                "Email error does not exist");
+        assertEquals(contactPage.getMessageErrorText(), "",
+                "Message error does not exist");
     }
 
     @Test
     public void testCase03() {
-        // Going to the contact page
         ContactPage contactPage = homePage.clickContactPage();
-
-        // Populating mandatory fields
         contactPage.setForename("ex");
         contactPage.setEmail("example@gmail.com");
         contactPage.setMessage("I am new!");
-
-        // Clicking the submit button
         contactPage.clickSubmitButton();
-
-        // Verifying "Thanks" message text
-        assertEquals(contactPage.getAlertText(), "Thanks ex, we appreciate your feedback.");
+        assertEquals(contactPage.getAlertText(), "Thanks ex, we appreciate your feedback.",
+                "Alert text appears");
     }
 }
