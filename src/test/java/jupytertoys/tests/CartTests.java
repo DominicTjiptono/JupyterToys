cff450f6cfc803f890ce77530fae39fc004c83d3
Dominic Tjiptono <dtjiptono@planittesting.com>
2022-01-18 16:20:46 +1300

    Added assertion messages to test case 10.

--- src/test/java/jupytertoys/tests/CartTests.java
+++ src/test/java/jupytertoys/tests/CartTests.java
@@ -6,31 +6,36 @@ import jupytertoys.pages.ShopPage;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertNotEquals;
+import static org.testng.Assert.assertNotNull;
+
 
 public class CartTests extends BaseTests {
     @Test
     public void testCase10() {
         ShopPage shopPage = homePage.clickShopPage();
         Product stuffedFrog = shopPage.getProduct(p -> p.getTitle().equals("Stuffed Frog"));
-        assertNotEquals(stuffedFrog, null);
+        assertNotNull(stuffedFrog, "Stuffed Frog not found on Shop Page!");
         Product fluffyBunny = shopPage.getProduct(p -> p.getTitle().equals("Fluffy Bunny"));
-        assertNotEquals(fluffyBunny, null);
+        assertNotNull(fluffyBunny, "Fluffy Bunny not found on Shop Page!");
         Product valentineBear = shopPage.getProduct(p -> p.getTitle().equals("Valentine Bear"));
-        assertNotEquals(valentineBear, null);
+        assertNotNull(valentineBear, "Valentine Bear not found on Shop Page!");
         stuffedFrog.buyProduct(2);
         fluffyBunny.buyProduct(5);
         valentineBear.buyProduct(3);
         CartPage cartPage = shopPage.clickCartPage();
-        assertEquals(cartPage.getNthProductPrice(1), 10.99);
-        assertEquals(cartPage.getNthProductPrice(2), 8.99);
-        assertEquals(cartPage.getNthProductPrice(3), 13.99);
+        assertEquals(cartPage.getNthProductPrice(1), 10.99,
+                "Invalid Stuffed Frog price.");
+        assertEquals(cartPage.getNthProductPrice(2), 8.99,
+                "Invalid Fluffy Bunny price.");
+        assertEquals(cartPage.getNthProductPrice(3), 13.99,
+                "Invalid Valentine Bear price.");
         assertEquals(cartPage.getNthProductPrice(1) * cartPage.getNthProductQuantity(1),
-                cartPage.getNthProductSubtotal(1));
+                cartPage.getNthProductSubtotal(1), "Invalid Stuffed Frog subtotal.");
         assertEquals(cartPage.getNthProductPrice(2) * cartPage.getNthProductQuantity(2),
-                cartPage.getNthProductSubtotal(2));
+                cartPage.getNthProductSubtotal(2), "Invalid Fluffy Bunny subtotal.");
         assertEquals(cartPage.getNthProductPrice(3) * cartPage.getNthProductQuantity(3),
-                cartPage.getNthProductSubtotal(3));
-        assertEquals(cartPage.getCalculatedTotal(), cartPage.getActualTotal());
+                cartPage.getNthProductSubtotal(3), "Invalid Valentine Bear subtotal.");
+        assertEquals(cartPage.getCalculatedTotal(), cartPage.getActualTotal(),
+                "Invalid calculated total price.");
     }
 }
